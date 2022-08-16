89f93d2d34b2e6194d0031d45376567faf068d61
CotterellAnna <cotterellanna@gmail.com>
2022-08-16 14:32:15 +0100

    using jQuery AJAX

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-let windspeedEl = document.getElementById("windspeed-el")
-let tempEl = document.getElementById("temp-el")
-let humidEl = document.getElementById("humid-el")
-let pressureEl = document.getElementById("pressure-el")
-let city = document.getElementById("city")
-let input = document.getElementById("input-form")
-let inputContainer = document.getElementById("input-container")
-let cityEL = document.getElementById("city-el")
-
-input.onsubmit = (e) => {
-    e.preventDefault()
-    weatherUpdate(city.value)
-}
-
-function weatherUpdate(city){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cad7ec124945dcfff04e457e76760d90&units=metric`)
-    .then(res => {
-        if (res.status === 404){
-            alert("Place not found")
-            return 0
-        }else if (res.status === 200){
-            return (res.json())
-        }
+$(function(){
+    $("#input-form").submit(function(e){
+        e.preventDefault()
+        weatherUpdate($("#city").val())
     })
-    .then(response =>{
-        windspeedEl.innerHTML = (response.wind.speed*3.6).toFixed(2)+`km/h`
-        tempEl.textContent = `${Math.round(response.main.temp)}°C`
-        humidEl.innerHTML = `${response.main.humidity}%`
-        pressureEl.textContent = `${response.main.pressure}hPa`
-        inputContainer.style.backgroundImage=`url(https://open.mapquestapi.com/staticmap/v4/getmap?key=UtdZoRly3u38HB9RSlLNJd2cAD82KsYK&size=600,400&zoom=13&center=${response.coord.lat},${response.coord.lon})`
-        cityEL.innerHTML = city
-    })
-}
-
-weatherUpdate(city.value)
+    function weatherUpdate(city){
+        $.ajax({
+            url:`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cad7ec124945dcfff04e457e76760d90&units=metric`,
+            complete: function(res, status){
+                
+                if(status=="success"){
+                    let data = res.responseJSON
+                    $("#windspeed-el").html((data.wind.speed*3.6).toFixed(2)+`km/h`)
+                    $("#temp-el").text(`${Math.round(data.main.temp)}°C`)
+                    $("#humid-el").html(`${data.main.humidity}%`)
+                    $("#pressure-el").text(`${data.main.pressure}hPa`)
+                    $("#input-container").css("backgroundImage",`url(https://open.mapquestapi.com/staticmap/v4/getmap?key=UtdZoRly3u38HB9RSlLNJd2cAD82KsYK&size=600,400&zoom=13&center=${data.coord.lat},${data.coord.lon})`)
+                    $("#city-el").html(city)
+                }else{
+                    alert("Place not found")
+                }
+            }
+        })
+    }
+    weatherUpdate($("#city").val())
+})
